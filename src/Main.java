d00b7dd07000a49eb9ad682ecdd4ab80d213b12a
Antonioni17 <antonioni17@yandex.ru>
2023-02-18 14:32:32 +0300

    newapp1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticketprice = 2000;
+        int bonusprice = 256;
+         int bonus =  ticketprice / bonusprice;
+         System.out.print ("Бонусных миль : " + bonus);
     }
 }
